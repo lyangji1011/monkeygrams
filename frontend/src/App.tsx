83adf0ae5c102e3590c5ba89c38f3da91c3276d0
Lucian Tash <30455406+luciankt@users.noreply.github.com>
2026-07-11 22:38:24 -0700

    home page button styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,20 @@
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 import HomePageButtons from './components/HomePageButtons'
 
 function App() {
-  const [message, setMessage] = useState('')
+  // const [message, setMessage] = useState('')
 
-  useEffect(() => {
-    fetch('/api/message')
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message))
-      .catch((err) => console.error(err))
-  }, [])
+  // useEffect(() => {
+  //   fetch('/api/message')
+  //     .then((res) => res.json())
+  //     .then((data) => setMessage(data.message))
+  //     .catch((err) => console.error(err))
+  // }, [])
 
   return (
     <div>
       <h1>MonkeyGrams</h1>
       <HomePageButtons />
-      <p>Backend says: {message || "Loading..."}</p>
     </div>
   )
 }
--- frontend/src/components/HomePageButtons.tsx
+++ frontend/src/components/HomePageButtons.tsx
@@ -1,9 +1,19 @@
 function HomePageButtons() {
+
+  function typeRoomCode(event: React.ChangeEvent<HTMLInputElement>) {
+    const roomCode = event.target.value.toUpperCase();
+    event.target.value = roomCode;
+  }
+
   return (
     <div>
-      <input type="text" placeholder="Room Code" />
-      <button>Join</button>
-      <button>Create Room</button>
+      <div className="join-room">
+        <input type="text" placeholder="Room Code" maxLength={4} onChange={typeRoomCode} />
+        <button>Join</button>
+      </div>
+      <div className="create-room">
+        <button>Create Room</button>
+      </div>
     </div>
   )
 }
